826644a5f1e15307fca7e522ab71a8b40bf8c1d0
Emirlan07055 <122383421+Emirlan07055@users.noreply.github.com>
2023-01-10 21:57:04 +0600

    выполнил дз2

---
+++ DZ2_Emirlan_22-01/main.js
@@ -0,0 +1,26 @@
+// let lst=[10, 20, 30, 40, 50]
+// for (element of lst){
+//     if (String(element)[0]==="1"){
+//         console.log(element)
+//     }else if (String(element)[0]==="2"){
+//         console.log(element)
+//     }else if (String(element)[0]==="5"){
+//         console.log(element)
+//     }
+// }
+
+/*****************************/
+
+// for (let i = 20; i >= 0; i--) {
+//     console.log(i)
+// }
+
+/*****************************/
+
+// let colors = prompt('Введите цвет')
+// let obj = {'red': 'STOP', 'yellow': 'Приготовтесь', 'green': 'Вперед'}
+// if (colors in obj){
+//     console.log(obj[colors])
+// }else {
+//     alert('ВВЕДИТЕ ЦВЕТ !!!')
+// }
